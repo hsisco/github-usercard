07ca13c9497efc615bc0baa25e6a39d2f6175484
hsisco <hysensisco@gmail.com>
2019-10-04 13:15:06 -0700

    gitCard function elements

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -11,22 +11,6 @@ followersArray.forEach(user => {
     console.log('Nope. Try again.', error)})
 });
 
-function gitCard(user) {
-  const 
-    card = document.createElement('div'),
-    imageURL = document.createElement('img'),
-    cardInfo = document.createElement('div'),
-    name = document.createElement('h3'),
-    userName = document.createElement('p'),
-    location = document.createElement('p'),
-    profile = document.createElement('p'),
-    profileURL = document.createElement('a'),
-    followers = document.createElement('p'),
-    following = document.createElement('p'),
-    bio = document.createElement('p');
-
-    
-}
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -40,30 +24,17 @@ function gitCard(user) {
 
 const followersArray = [];
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+function gitCard(user) {
+  const 
+    card = document.createElement('div'),
+    imageURL = document.createElement('img'),
+    cardInfo = document.createElement('div'),
+    name = document.createElement('h3'),
+    userName = document.createElement('p'),
+    location = document.createElement('p'),
+    profile = document.createElement('p'),
+    profileURL = document.createElement('a'),
+    followers = document.createElement('p'),
+    following = document.createElement('p'),
+    bio = document.createElement('p');
+}
